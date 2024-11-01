6d85c168552b63caa93bac26e85ce5867dfc2058
Vivekanand Mendhe <vivek.mendhe.022@gmail.com>
2024-11-01 11:58:36 +0530

    remote commit

---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.scss']
+})
+export class CounterComponent {
+  count = 0;
+
+  increment() {
+    this.count++;
+  }
+
+  decrement() {
+    if (this.count > 0) {
+      this.count--;
+    }
+  }
+}
